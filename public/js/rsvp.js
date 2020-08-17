527079c74d32d63ef0a9c67c496af9a067ad97d4
Lukas Atkinson <fra-uas@LukasAtkinson.de>
2020-08-17 12:40:43 +0200

    RSVP reader: separate logic from UI more clearly
    And some various related changes:
    
    * rewrite as a JavaScript module (import/export)
    * extract state machine/logic as `RsvpModel` class
    * handle DOM manipulation in `initializeUI()` function
    * ask user to explicitly start the playback
    * show a message beneath the current word if playback is paused
    * show an empty square to mark the end of playback
    * remove commented-out code that referred to specific speed levels

--- public/js/rsvp.js
+++ public/js/rsvp.js
@@ -1,229 +1,275 @@
-let rsvpModule = (function () {
-  let playing = false; //shows whether we are playing or not
-  let index = 0; //at which word are we in the array
-  let wpm; //wpm
-  let interval; //the reference returned by the setInterval function, to stop or play
-  //let speeds = [150, 250, 300, 450, 575, 675, 800]; //wpms
-  //let speedIndex = 0;
+// @ts-check
+// eslint-env browser,es2017
 
-  let countSlower = 0;
-  let countFaster = 0;
-  let countPauses = 0;
-
-  let startTime;
-  var seconds;
-
-  let finished = false;
-  let nextButton = document.getElementById("nextButton");
-
-  let manual = false;
-
-  let arrayOfWords = [];
-  wordEl = document.getElementById("word"); //Html element that will display the words.
-
-  wpmLabel = document.getElementById("wpmLabel");
-
-  //init
-  function init(words, wordsPerMinute) {
-    arrayOfWords = words;
-    wpm = wordsPerMinute;
+class Timer {
+  constructor() {
+    this.start = performance.now();
   }
 
-  function play() {
-    if (playing) return;
-    interval = setInterval(playerOn, (60 * 1000) / wpm); //calls the playerOn function according to the speed.
-    playing = true;
-    nextButton.style.display = "none";
+  elapsedMS() {
+    return performance.now() - this.start;
+  }
+}
+
+/**
+ * Logic for an abstract RSVP reader, does not contain UI elements.
+ *
+ * States:
+ *
+ * * paused (initial)
+ * * playing
+ * * finished
+ *
+ * State transitions:
+ *
+ * * play: paused -> playing
+ * * pause: playing -> paused, count this event
+ * * stop: playing -> paused, do not count the event
+ * * _advanceToNextToken: any -> any, but may reach the finished state
+ */
+export class RsvpModel {
+  /**
+   * Create a new RSVP model.
+   *
+   * Remember to call play() to start the playback.
+   *
+   * @param {object} config
+   * @param {string[]} config.tokens the words to be shown
+   * @param {number} config.wpm the initial speed
+   * @param {(token: string) => void} config.onNewToken called to show each token
+   * @param {(elapsedTimeMS: number) => void} config.onComplete called after the last token has been read
+   * @param {(wpm: number) => void} config.onWpmChange called when the speed is changed
+   */
+  constructor({ tokens, wpm, onNewToken, onComplete, onWpmChange }) {
+    this.tokens = tokens;
+    this.wpm = wpm;
+    this.playing = null;
+    this.timer = null;
+    this.tokenIndex = 0;
+    this.onNewToken = onNewToken;
+    this.onComplete = onComplete;
+    this.onWpmChange = onWpmChange;
+
+    this.counts = {
+      slower: 0,
+      faster: 0,
+      pause: 0,
+      forward: 0,
+      rewind: 0,
+    };
+
+    // initialize the caller's state with the first word
+    this._emitNewTokenEvent();
+    // initialize the caller's state with the WPM display
+    this.onWpmChange(wpm);
   }
 
-  function stop() {
-    if (!playing) return;
-    clearInterval(interval); //clears the interval
-    playing = false;
-    if (finished) {
-      nextButton.style.display = "block";
-    }
+  /**
+   * true if no words remain to be read
+   */
+  isFinished() {
+    return !(this.tokenIndex < this.tokens.length);
   }
 
-  function reset() {
-    if (playing) {
-      stop();
+  /**
+   * calls the user-supplied callback with the current word
+   */
+  _emitNewTokenEvent() {
+    let i = this.tokenIndex;
+    // terminating token is "white square"
+    let token = i < this.tokens.length ? this.tokens[i] : "\u25A1";
+    this.onNewToken(token);
+  }
+
+  /**
+   * move forward by one word, then _emitNewTokenEvent().
+   *
+   * If no words remain, stop() and emit onComplete().
+   */
+  _advanceToNextToken() {
+    if (this.isFinished()) {
+      this.stop();
+      this.onComplete(this.timer.elapsedMS());
     } else {
-      play();
+      this.tokenIndex++;
+      this._emitNewTokenEvent();
     }
   }
 
-  function forward() {
-    stop();
-    if (index < arrayOfWords.length - 1) {
-      index++;
-      wordEl.innerHTML = arrayOfWords[index];
-      if (index == arrayOfWords.length - 1) {
-        finished = true;
-        stopTimer();
-        nextButton.style.display = "block";
-      }
+  /**
+   * resume or start playing
+   */
+  play() {
+    if (this.playing) return;
+    let refreshEveryMS = (60 * 1000) / this.wpm;
+    this.playing = setInterval(() => {
+      this._advanceToNextToken();
+    }, refreshEveryMS);
+    if (!this.timer) this.timer = new Timer();
+  }
+
+  /**
+   * stop playback but register it as a pause
+   */
+  pause() {
+    if (!this.playing) return;
+    this.counts.pause++;
+    this.stop();
+  }
+
+  /**
+   * stop playback
+   */
+  stop() {
+    if (this.playing) {
+      clearInterval(this.playing);
+      this.playing = null;
     }
   }
 
-  function rewind() {
-    stop();
-    if (index > 0) {
-      index--;
-      if (index == 0) {
-        wordEl.innerHTML = arrayOfWords[0];
-      } else {
-        wordEl.innerHTML = arrayOfWords[index - 1];
-      }
-    }
-  }
-
-  function playerOn() {
-    if (index >= arrayOfWords.length) {
-      finished = true;
-      stopTimer();
-      stop();
+  /**
+   * toggle between play() and pause()
+   */
+  toggle() {
+    if (this.playing) {
+      this.pause();
     } else {
-      wordEl.innerHTML = arrayOfWords[index];
-      index++;
+      this.play();
     }
+  }
+
+  _withSuspendedPlayback(callback) {
+    if (this.playing) {
+      this.stop();
+      callback();
+      this.play();
+    } else {
+      callback();
+    }
+  }
+
+  /**
+   * skip forward by one token
+   */
+  forward() {
+    this.stop();
+    this.counts.forward++;
+    this._advanceToNextToken();
+  }
+
+  /**
+   * skip backwards by one token
+   */
+  rewind() {
+    this.stop();
+    if (this.tokenIndex > 0) this.tokenIndex--;
+    this.counts.rewind++;
+    this._emitNewTokenEvent();
+  }
+
+  /**
+   * increase playback speed
+   */
+  faster() {
+    this._withSuspendedPlayback(() => {
+      this.wpm += 10;
+      this.counts.faster++;
+      this.onWpmChange(this.wpm);
+    });
+  }
+
+  /**
+   * reduce playback speed
+   */
+  slower() {
+    this._withSuspendedPlayback(() => {
+      // never go below 100 WPM
+      const newWpm = Math.max(100, this.wpm - 10);
+      if (this.wpm == newWpm) return;
+      this.wpm = newWpm;
+      this.counts.slower++;
+      this.onWpmChange(newWpm);
+    });
+  }
+}
+
+/**
+ * connect existing DOM elements to a new RSVP model.
+ *
+ * Required elements: `#nextButton, #word, #wpmLabel, .mainReader`
+ */
+export function initializeUI(tokens, wpm) {
+  let $nextButton = document.getElementById("nextButton");
+  $nextButton.style.display = "none";
+
+  let $word = document.getElementById("word");
+
+  let $wpmLabel = document.getElementById("wpmLabel");
+
+  let $message = document.getElementById("message");
+  $message.textContent = "press SPACE to start";
+
+  /** @type {HTMLElement} */
+  // @ts-ignore
+  let $mainReader = document.getElementsByClassName("mainReader")[0];
+
+  let rsvp = new RsvpModel({
+    tokens,
+    wpm,
+    onNewToken: (word) => {
+      $word.textContent = word;
+    },
+    onComplete: (elapsedMS) => {
+      let time = Math.round(elapsedMS / 1000); // convert to seconds
+
+      let {
+        slower: cs,
+        faster: cf,
+        pause: cp,
+        forward: cff,
+        rewind: crr,
+      } = rsvp.counts;
+
+      $nextButton.style.display = "block";
+      $nextButton.setAttribute(
+        "href",
+        `/form?cf=${cf}&cs=${cs}&cp=${cp}&cff=${cff}&crr=${crr}&t=${time}`
+      );
+    },
+    onWpmChange: (wpm) => {
+      $wpmLabel.textContent = `words/minute: ${wpm} wpm`;
+    },
+  });
+
+  function updateMessage(isPlaying) {
+    $message.textContent = isPlaying ? "" : "press SPACE to resume";
   }
 
   //clicking on the word stops or resumes
-  document.getElementsByClassName("mainReader")[0].onclick = function () {
-    if(playing)
-    {
-      countPauses++;
-    }
-    reset();
+  $mainReader.onclick = function () {
+    rsvp.toggle();
+    updateMessage(rsvp.playing);
   };
 
+  // support keyboard controls
   document.onkeydown = function (event) {
     switch (event.key) {
       case "ArrowUp":
-        faster();
+        rsvp.faster();
         break;
       case "ArrowDown":
-        slower();
+        rsvp.slower();
         break;
       case "ArrowLeft":
-        rewind();
+        rsvp.rewind();
         break;
       case " ":
-        if(playing)
-        {
-          countPauses++;
-        }
-        reset();
+        rsvp.toggle();
+        updateMessage(rsvp.playing);
         break;
       case "ArrowRight":
-        forward();
+        rsvp.forward();
         break;
     }
   };
-
-  function faster() {
-    wpm += 10;
-    updateLabel();
-    adjustSpeed();
-    countFaster++;
-    // if (!manual) {
-    //   //first time to adjust speed
-    //   manual = true;
-    //   speedsNr = speeds.length;
-    //   for (let i = 0; i < speedsNr; i++) {
-    //     if (wpm < speeds[i]) {
-    //       wpm = speeds[i];
-    //       speedIndex = i;
-    //       updateLabel();
-    //       adjustSpeed();
-    //       break;
-    //     }
-    //   }
-    // } else {
-    //   speedIndex++;
-    //   if (speedIndex < speeds.length) {
-    //     wpm = speeds[speedIndex];
-    //     updateLabel();
-    //     adjustSpeed();
-    //   } else {
-    //     speedIndex = speeds.length - 1;
-    //   }
-    // }
-  }
-
-  function slower() {
-    wpm -= 10;
-    if (wpm < 100) {
-      wpm = wpm + 10;
-    }
-    else
-    {
-      countSlower++;
-      updateLabel();
-      adjustSpeed();
-    }
-    
-    // if (!manual) {
-    //   //first time to adjust speed
-    //   manual = true;
-    //   speedsNr = speeds.length;
-    //   for (let i = speedsNr-1; i >= 0; i--) {
-    //     if (wpm > speeds[i]) {
-    //       wpm = speeds[i];
-    //       speedIndex = i;
-    //       updateLabel();
-    //       adjustSpeed();
-    //       break;
-    //     }
-    //   }
-    // } else {
-    //   speedIndex--;
-    //   if (speedIndex >= 0) {
-    //     wpm = speeds[speedIndex];
-    //     updateLabel();
-    //     adjustSpeed();
-    //   } else {
-    //     speedIndex = 0;
-    //   }
-    // }
-  }
-
-  function adjustSpeed() {
-    if (playing) {
-      stop();
-      play();
-    }
-  }
-
-  function updateLabel() {
-    wpmLabel.innerHTML = "Words/minute: " + wpm;
-  }
-  //self explanatory
-  window.onload = function () {
-    updateLabel();
-    setTimeout(play, 600);
-    startTime = performance.now();
-  };
-
-  function stopTimer() {
-    let endTime = performance.now();
-    var timeDiff = endTime - startTime - 600; //in ms
-    // strip the ms
-    timeDiff /= 1000;
-    // get seconds
-    seconds = Math.round(timeDiff);
-  }
-  function getData() {
-    return {
-      countFaster: countFaster,
-      countSlower: countSlower,
-      time: seconds,
-      countPauses: countPauses
-    };
-  }
-  return {
-    init: init,
-    getData: getData,
-  };
-})();
+}
